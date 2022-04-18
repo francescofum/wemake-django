231da850b19a71055026141aaab478199570e351
francesco.fum <francesco.fum@gmail.com>
2022-04-18 22:08:19 +0100

    no comment

--- materials/views.py
+++ materials/views.py
@@ -8,4 +8,14 @@ from .forms import  MaterialForm
 
 
 def material_details(request,id:int=None): 
-    pass
+
+    # Either display an existing material,
+    # or a blank form for a new material.
+    if request.method == "GET":
+        if id is not None:
+            pass
+            # material = Printer.objects.get(pk=id)
+            # printer_form = PrinterForm(instance = printer,prefix="printer")
+            # material_form = MaterialForm(vendor=vendor,printer=printer,prefix="material")
+        else:
+            material_form = MaterialForm()
