67333e2af86f18cb9b5cc6f0cdd18948138d2a3d
francesco.fum <francesco.fum@gmail.com>
2022-04-18 21:15:08 +0100

    should be in new branch switching over..

--- materials/forms.py
+++ materials/forms.py
@@ -1,4 +1,18 @@
+from django import forms
+from django.forms.widgets import CheckboxSelectMultiple
 
+from .models import MaterialOptions
+from core.models import MATERIAL_GLOBAL
+from core.models import COLOUR_GLOBAL
+
+
+class MaterialForm(forms.ModelForm):
+    '''
+        @brief TODO
+    '''
+    class Meta:
+        model = MaterialOptions
+        fields = '__all__' 
 
 
 
--- materials/views.py
+++ materials/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
 
+from materials.models import MaterialOptions
+from .models import Printer
+from vendor.models import Vendor
+from .forms import  MaterialForm
+
+
+def material_details(request,id:int=None): 
+    pass
