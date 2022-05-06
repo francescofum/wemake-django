347676d02353e629b523ea704e49791ed171c79c
francesco.fum <francesco.fum@gmail.com>
2022-05-06 22:44:38 +0100

    Dockerized django
    - requirements.txt updated
    - settings are can now be changed from env variables

--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 def frontpage(request):
+    response = requests.post('http://wm_slicer_cura:5555/cura', json={"one": 1})
+    print(response.text)
     return render(request,'core/frontpage.html')
 
 def checkout(request):
