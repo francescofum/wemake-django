f4f78753d0deef0d7e18684829328bbff369c734
Raul Arribas <raul@Rauls-MacBook-Pro.local>
2022-05-08 21:23:43 +0100

    checkout redirect

--- static/js/print_preview/print_preview.js
+++ static/js/print_preview/print_preview.js
@@ -69,16 +69,12 @@ function add_all_items_to_cart() {
         type: 'post',
         data:{'stl_list': JSON.stringify(stl_list)},
         success: function (response) {
-            console.log('here');
-            var url = `${window.location.origin}/checkout`;
-            console.log(url)
-            // window.location.assign(url);
-
+            window.location.replace('/vendor/order/checkout');
         },
         error: function (response) {
             console.log('error');
             console.log(url)
-        }
+        },
         headers: {
             'X-CSRFToken': csrftoken
         }
