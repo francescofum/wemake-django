a1674cf41dc9fd1f110e1777245681d3e21102af
francesco.fum <francesco.fum@gmail.com>
2022-10-04 20:32:50 +0100

    WP-99 Fixed images in print preview
    - added contact us backend

--- manage.py
+++ manage.py
@@ -2,11 +2,20 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+import debugpy
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wemake.settings')
+
+    try:
+        
+        debugpy.listen(("0.0.0.0", 3002))
+        debugpy.wait_for_client()
+        print('Attached!')
+    except Exception as e:
+        pass
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
